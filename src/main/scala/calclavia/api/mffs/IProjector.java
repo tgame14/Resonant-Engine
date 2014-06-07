2541233e517303641fe04a3ac421f12c33a29d04
Calclavia <admin@calclavia.com>
2014-06-08 00:21:33 +0800

    IProjector now implements IBlockFrequency

--- src/main/scala/calclavia/api/mffs/IProjector.java
+++ src/main/scala/calclavia/api/mffs/IProjector.java
@@ -3,12 +3,13 @@ package calclavia.api.mffs;
 import java.util.Set;
 
 import net.minecraft.inventory.IInventory;
+import resonant.api.blocks.IBlockFrequency;
 import universalelectricity.api.vector.Vector3;
 
 /** Also extends IDisableable, IFortronFrequency
  * 
  * @author Calclavia */
-public abstract interface IProjector extends IInventory, IBiometricIdentifierLink, IFieldInteraction
+public abstract interface IProjector extends IInventory, IBiometricIdentifierLink, IFieldInteraction, IBlockFrequency
 {
     /** Projects a force field. */
     public void projectField();
@@ -20,9 +21,9 @@ public abstract interface IProjector extends IInventory, IBiometricIdentifierLin
     public int getProjectionSpeed();
 
     /** * @return The amount of ticks this projector has existed in the world. */
-    public long getTicks();
+	public long getTicks();
 
-    /** DO NOT modify this list. Read-only.
+	/** DO NOT modify this list. Read-only.
      * 
      * @return The actual force field block coordinates in the world. */
     public Set<Vector3> getForceFields();
